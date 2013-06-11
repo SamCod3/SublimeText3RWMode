4cd2c6a6747adbf21a015743e654227ec32a6a98
Luis Medinas <lmedinas@gmail.com>
2013-06-11 11:55:37 +0300

    Initial import of ST3 RW Mode plugin

---
+++ rwmodest3.py
@@ -0,0 +1,13 @@
+import sublime
+import sublime_plugin
+import os
+ 
+class WriteCommand(sublime_plugin.TextCommand):
+        def run(self, edit):
+                File = self.view.file_name()
+                os.chmod(File, stat.S_IWUSR)
+ 
+class ReadCommand(sublime_plugin.TextCommand):
+        def run(self, edit):
+                File = self.view.file_name()
+                os.chmod(File, stat.S_IRUSR)
