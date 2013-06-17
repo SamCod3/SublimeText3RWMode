78eb670e099eea18012d3886bf7fa543edf3351e
Luis Medinas <lmedinas@gmail.com>
2013-06-17 12:15:51 +0300

    Update rwmodest3.py
    Add correct imports

--- rwmodest3.py
+++ rwmodest3.py
@@ -1,7 +1,10 @@
 import sublime
 import sublime_plugin
+
 import os
- 
+import sys
+import stat
+
 class WriteCommand(sublime_plugin.TextCommand):
         def run(self, edit):
                 File = self.view.file_name()
